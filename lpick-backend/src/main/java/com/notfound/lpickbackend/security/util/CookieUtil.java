0b77c4c484d24bda04288cb2b94e62e89393a1ad
JungUiJin <yealkki38@gmail.com>
2025-07-19 13:53:19 +0900

    update36

--- lpick-backend/src/main/java/com/notfound/lpickbackend/security/util/CookieUtil.java
+++ lpick-backend/src/main/java/com/notfound/lpickbackend/security/util/CookieUtil.java
@@ -2,31 +2,37 @@ package com.notfound.lpickbackend.security.util;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseCookie;
 
 /*
 * Cookie 생성과 삭제를 담당할 Util 클래스
 * 코드 재사용성과 유지보수를 위해 작성
 * */
 public class CookieUtil {
-    
-    // 쿠키 추가 메소드
+
+    // 쿠키 추가
     public static void addCookie(HttpServletResponse response, String name, String value, int maxAgeInSec) {
-        Cookie cookie = new Cookie(name, value);
-        cookie.setHttpOnly(true);
-        cookie.setSecure(false); // 테스트 단계이기 때문에 false로 설정
-        cookie.setPath("/");
-        cookie.setMaxAge(maxAgeInSec);
-        response.addCookie(cookie);
+        ResponseCookie cookie = ResponseCookie.from(name, value)
+                .httpOnly(true)
+                .secure(false) // 추후 NGINX 등 https 설정 추가시 수정
+                .sameSite("None") // Cross-Origin 허용
+                .path("/")
+                .maxAge(maxAgeInSec)
+                .build();
+
+        response.addHeader("Set-Cookie", cookie.toString());
     }
-    
-    // 쿠키 삭제 메소드
+
+    // 쿠키 삭제
     public static void deleteCookie(HttpServletResponse response, String name) {
-        
-        Cookie cookie = new Cookie(name, null);
-        cookie.setPath("/");
-        cookie.setMaxAge(0); // 즉시 만료
-        cookie.setHttpOnly(true);
-        cookie.setSecure(false);
-        response.addCookie(cookie);
+        ResponseCookie cookie = ResponseCookie.from(name, "")
+                .httpOnly(true)
+                .secure(false)
+                .sameSite("None")
+                .path("/")
+                .maxAge(0)
+                .build();
+
+        response.addHeader("Set-Cookie", cookie.toString());
     }
 }
