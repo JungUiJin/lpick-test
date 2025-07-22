1371506bc28c34bd8eedb2998f50f03b2a36a3a2
JungUiJin <yealkki38@gmail.com>
2025-07-23 01:05:23 +0900

    update48

--- lpick-backend/src/main/java/com/notfound/lpickbackend/LpickBackendApplication.java
+++ lpick-backend/src/main/java/com/notfound/lpickbackend/LpickBackendApplication.java
@@ -1,11 +1,14 @@
 package com.notfound.lpickbackend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "https://lpick.duckdns.org", description = "Default Server URL")})
 public class LpickBackendApplication {
 
     public static void main(String[] args) {
